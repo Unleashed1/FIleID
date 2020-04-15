ed680a26e9dbbf47d36acca6e09786853f15172c
unleashed1 <mastrogiacomo.1748008@studenti.uniroma1.it>
2020-04-15 23:35:10 +0200

    added the rename of the files

--- BotFile.py
+++ BotFile.py
@@ -1,15 +1,15 @@
 import os
 import json
 file_path = os.getcwd() ##path filejson
-filelist = os.listdir(os.getcwd())
-
+path = os.getcwd()+'\FileFolder'
+filelist = os.listdir(path)
+index = 0 
 out = []
-
 for el in filelist :
     print(el)
-    abspath = os.path.abspath(file_path) # path file for the examination
-    data = json.loads(open(os.path.join(abspath, file_path+"/data.json"), "r",encoding = "utf-8").read())
-    file = open(file_path+'/'+el, "rb").read(32) #to mod
+    abspath = os.path.abspath(path) # path file for the examination
+    data = json.loads(open(os.path.join(file_path, file_path+"/data.json"), "r",encoding = "utf-8").read())
+    file = open(path+'/'+el, "rb").read(32) #to mod
     hex_bytes = " ".join(['{:02X}'.format(byte) for byte in file])
     
     for element in data:
@@ -17,5 +17,10 @@ for el in filelist :
             offset = element["offset"]*2+element["offset"]
             if signature == hex_bytes[offset:len(signature)+offset].upper():
                 out.append(element["extension"])
+
 print(out) #to mod 
 #i'm going to add the automatic aquisition of the paths and the restore sistem for all  the files in the directory
+os.chdir(os.getcwd()+'\FileFolder')
+for elem in filelist:
+    os.rename(path+'/'+elem,'file'+str(index)+'.'+out[index+1])
+    index= index+2
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # FileID
-This bot will identify the extension of the files in the folder, which are assumed to have the wrong extension i'm going to modify and add function soon
+This bot will identify and modify the extension of the files in the folder
